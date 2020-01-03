92e6ab439ee05ad2f9869cc32ee96df03665072c
lemoyuan <lin_lemo@163.com>
2020-01-03 05:54:26 +0000

    Deploying to gh-pages from master b6a3d093fc1508a5b0e908be2f0e9617ffe84071

---
+++ precache-manifest.6a8b48d09d47d9feb4217eed097b9328.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "78404b4b0f010a1ef451",
+    "url": "/react-demo/static/js/runtime~main.3dcdfef1.js"
+  },
+  {
+    "revision": "2a71c8cb3016206c048c",
+    "url": "/react-demo/static/js/main.19d3abf1.chunk.js"
+  },
+  {
+    "revision": "bb7f4862e99ac82ff16c",
+    "url": "/react-demo/static/js/2.67da9ef2.chunk.js"
+  },
+  {
+    "revision": "2a71c8cb3016206c048c",
+    "url": "/react-demo/static/css/main.f4b735cb.chunk.css"
+  },
+  {
+    "revision": "bb7f4862e99ac82ff16c",
+    "url": "/react-demo/static/css/2.65a47aa8.chunk.css"
+  },
+  {
+    "revision": "de292328792b214f8b4437c463140c5f",
+    "url": "/react-demo/index.html"
+  }
+];
